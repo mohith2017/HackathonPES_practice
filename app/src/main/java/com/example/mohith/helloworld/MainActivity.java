866bec8287fb3bbce6fda46b3cb97c9229462a8c
kslohith <32676813+kslohith@users.noreply.github.com>
2017-12-17 00:34:02 +0530

    Lohith dev (#6)
    * hi
    
    * second page
    
    * second page
    
    * second page

--- app/src/main/java/com/example/mohith/helloworld/MainActivity.java
+++ app/src/main/java/com/example/mohith/helloworld/MainActivity.java
@@ -1,20 +1,48 @@
 package com.example.mohith.helloworld;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+
 
 public class MainActivity extends AppCompatActivity {
-TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.game_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.location_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        Button b = (Button) findViewById(R.id.button_to_3);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent i = new Intent(MainActivity.this,page_3.class);
+                startActivity(i);
+            }
 
-        t=(TextView)findViewById(R.id.textView2);
 
-        t.setText("Let's ro!");
+        });
+
+
+
+
+
     }
+
 }
