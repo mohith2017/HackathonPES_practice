e1829d323ff4eb95b1fd5e459f28579c45fed599
kslohith <kslohith1729@gmail.com>
2017-12-17 00:24:48 +0530

    second page

--- app/src/main/java/com/example/mohith/helloworld/MainActivity.java
+++ app/src/main/java/com/example/mohith/helloworld/MainActivity.java
@@ -36,20 +36,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
 
-            class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
-
-                public void onItemSelected(AdapterView<?> parent, View view,
-                                           int pos, long id) {
-
-                    parent.getItemAtPosition(pos);
-
-                }
-
-                public void onNothingSelected(AdapterView<?> parent) {
-
-                }
-            }
         });
 
 
